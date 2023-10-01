6be984fc8a64232a098767a0a95f6e74b8c8edad
kunnu223 <kunalkumawat223@gmail.com>
2023-10-02 01:31:41 +0530

    final

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const DBconnect = require("./config/db");
-const path = require(" path");
+const path = require("path");
 
 dotenv.config();
 
